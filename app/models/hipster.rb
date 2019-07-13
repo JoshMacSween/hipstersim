347095e93d4ad688baaf5ca7b140759f56d938c8
Joshua MacSween <joshmacsween@gmail.com>
2019-07-13 16:08:14 -0600

    basic validations

--- app/models/hipster.rb
+++ app/models/hipster.rb
@@ -1,3 +1,6 @@
 class Hipster < ApplicationRecord
-
+  validates :name, presence: true
+  validates :beer, presence: true
+  validates :coffee, presence: true, length: { minimum: 10, maximum: 50 }
+  validates :quote, presence: true, length: { minimum: 10, maximum: 100 }
 end
