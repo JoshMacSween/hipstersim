c14907974db598114b8ec1bdb15ef5125cce2ccc
Joshua MacSween <joshmacsween@gmail.com>
2019-08-01 12:19:25 -0600

    added to migration

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,9 @@ ActiveRecord::Schema.define(version: 2019_07_31_215641) do
   enable_extension "plpgsql"
 
   create_table "hats", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.string "color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -25,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_07_31_215641) do
     t.string "beer"
     t.string "coffee"
     t.string "quote"
+    t.integer "hipster_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -35,6 +39,10 @@ ActiveRecord::Schema.define(version: 2019_07_31_215641) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
